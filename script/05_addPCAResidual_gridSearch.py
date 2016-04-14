2a532906bafea2d07ba5bbd26e29ccb22bfcd1be
EC2 Default User <ec2-user@ip-172-31-16-7.us-west-2.compute.internal>
2016-04-14 04:06:09 +0000

    add smarter grid search

--- script/05_addPCAResidual_gridSearch.py
+++ script/05_addPCAResidual_gridSearch.py
@@ -21,8 +21,6 @@ def addPCAFeature(df):
     print "pca_feature shape:", pca_feature.shape
     print "pca_residual shape:", pca_residual.shape
 
-#    for i in range(component_number):
-#        df['pca_feature' + str(i)] = pca_feature[:,i]
     for column in features.columns:
         df['pca_residual_' + column] = pca_residual[column]
 
@@ -40,15 +38,54 @@ convertToNumeric(df)
 
 addPCAFeature(df)
 
-param_grid = {
-            "n_estimators": [600, 800, 1000],
-            "max_features": [60, 70, 80],
-#            "min_samples_split": [4],
-            "max_depth": [40, 50],
+def auto_grid_search(data, target):
+    start_param_grid = {
+            "n_estimators": [850],
+            "max_features": [60],
+            "min_samples_split": [4],
+            "max_depth": [40],
             "min_samples_leaf": [2],
-            "criterion": ["entropy"]
-}
+    }
+    temp_param_grid = start_param_grid
+
+    best_score = -999
+    new_best_score = -99    
+
+    while (new_best_score - best_score > 0.0001): 
+        best_score = new_best_score 
+
+        # run grid search and compute new best param
+        clf = getModel()
+        searcher = GridSearchCV(clf, param_grid=temp_param_grid, scoring='log_loss', n_jobs=-1, pre_dispatch=2, verbose=6)
+        start = time()
+        searcher.fit(data, target)
+        print("GridSearchCV took %.2f seconds for %d candidate parameter settings."
+              % (time() - start, len(searcher.grid_scores_)))
+        print "Best parameter is: ", searcher.best_params_
+        print "Best score is:", searcher.best_score_
+        new_best_score = searcher.best_score_
+
+        # Generate new param grid from the best one.
+        new_param_grid = {}
+        for key in searcher.best_params_:
+            best_value = searcher.best_params_[key]
+            lower_value = int(best_value * 0.9)
+            higher_value = int(best_value * 1.1)  
+
+            new_values = []
+            if (lower_value is not 0):
+                new_values.append(lower_value)
+
+            new_values.append(best_value)
+     
+            if (higher_value is best_value):
+                new_values.append(best_value + 1)
+            else:
+                new_values.append(higher_value)
+
+            new_param_grid[key] = new_values
+
+        temp_param_grid = new_param_grid
 
 
-
-grid_search(df.drop(['ID','target'], axis=1), df['target']) 
+auto_grid_search(df.drop(['ID','target'], axis=1), df['target']) 
