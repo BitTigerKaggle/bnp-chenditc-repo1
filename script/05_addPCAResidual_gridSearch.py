f5b706cca3e06e0739ed91fb8a9793c05f193d08
EC2 Default User <ec2-user@ip-172-31-16-7.us-west-2.compute.internal>
2016-04-14 04:50:00 +0000

    tune algorithm to make search faster

--- script/05_addPCAResidual_gridSearch.py
+++ script/05_addPCAResidual_gridSearch.py
@@ -38,6 +38,22 @@ convertToNumeric(df)
 
 addPCAFeature(df)
 
+def get_neighbor_values(best_value):
+    lower_value = int(best_value * 0.9)
+    higher_value = int(best_value * 1.1)  
+
+    new_values = []
+    if (lower_value is not 0):
+        new_values.append(lower_value)
+
+    new_values.append(best_value)
+
+    if (higher_value is best_value):
+        new_values.append(best_value + 1)
+    else:
+        new_values.append(higher_value)
+    return new_values
+
 def auto_grid_search(data, target):
     start_param_grid = {
             "n_estimators": [850],
@@ -47,14 +63,25 @@ def auto_grid_search(data, target):
             "min_samples_leaf": [2],
     }
     temp_param_grid = start_param_grid
+    best_param_grid = {
+            "n_estimators": 3,
+            "max_features": 6,
+            "min_samples_split": 4,
+            "max_depth": 4,
+            "min_samples_leaf": 2,
+    }
+
 
     best_score = -999
     new_best_score = -99    
 
-    while (new_best_score - best_score > 0.0001): 
+    param_queue = start_param_grid.keys()
+
+    while (True): 
         best_score = new_best_score 
 
         # run grid search and compute new best param
+        print "Seaching params:", temp_param_grid
         clf = getModel()
         searcher = GridSearchCV(clf, param_grid=temp_param_grid, scoring='log_loss', n_jobs=-1, pre_dispatch=2, verbose=6)
         start = time()
@@ -67,25 +94,28 @@ def auto_grid_search(data, target):
 
         # Generate new param grid from the best one.
         new_param_grid = {}
-        for key in searcher.best_params_:
+
+        key = param_queue[0]
+        if searcher.best_params_[key] is not best_param_grid[key]:
+            # 1. If this was improved, keep searching
             best_value = searcher.best_params_[key]
-            lower_value = int(best_value * 0.9)
-            higher_value = int(best_value * 1.1)  
-
-            new_values = []
-            if (lower_value is not 0):
-                new_values.append(lower_value)
-
-            new_values.append(best_value)
-     
-            if (higher_value is best_value):
-                new_values.append(best_value + 1)
-            else:
-                new_values.append(higher_value)
-
+            new_values = get_neighbor_values(best_value)
+            new_param_grid[key] = new_values
+        else:
+            # 2. find the next one to tune
+            param_queue = param_queue[1:] + param_queue[:1]
+            key = param_queue[0] 
+            best_value = searcher.best_params_[key]
+            new_values = get_neighbor_values(best_value)
             new_param_grid[key] = new_values
 
+        # 3. Fill in best params for the rest 
+        for key in best_param_grid:
+            if key not in new_param_grid:
+                new_param_grid[key] = [best_param_grid[key]]
+
         temp_param_grid = new_param_grid
+        best_param_grid = searcher.best_params_
 
 
 auto_grid_search(df.drop(['ID','target'], axis=1), df['target']) 
