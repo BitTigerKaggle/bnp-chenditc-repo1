4bfc39f6298afc3ef01f351a4e746cce5cc91360
EC2 Default User <ec2-user@ip-172-31-10-101.us-west-2.compute.internal>
2016-04-15 01:39:37 +0000

    Fix param rotate bug

--- script/05_addPCAResidual_gridSearch.py
+++ script/05_addPCAResidual_gridSearch.py
@@ -38,9 +38,9 @@ convertToNumeric(df)
 
 addPCAFeature(df)
 
-def get_neighbor_values(best_value):
-    lower_value = int(best_value * 0.9)
-    higher_value = int(best_value * 1.1)  
+def get_neighbor_values(best_value, alpha):
+    lower_value = int(best_value * (1-alpha))
+    higher_value = int(best_value * (1+alpha))  
 
     new_values = []
     if (lower_value is not 0):
@@ -56,7 +56,7 @@ def get_neighbor_values(best_value):
 
 def auto_grid_search(data, target):
     start_param_grid = {
-            "n_estimators": [850],
+            "n_estimators": [1121],
             "max_features": [60],
             "min_samples_split": [4],
             "max_depth": [40],
@@ -64,26 +64,29 @@ def auto_grid_search(data, target):
     }
     temp_param_grid = start_param_grid
     best_param_grid = {
-            "n_estimators": 3,
-            "max_features": 6,
+            "n_estimators": 1121,
+            "max_features": 60,
             "min_samples_split": 4,
-            "max_depth": 4,
+            "max_depth": 40,
             "min_samples_leaf": 2,
     }
 
 
     best_score = -999
     new_best_score = -99    
+    alpha = 0.06
 
     param_queue = start_param_grid.keys()
 
     while (True): 
         best_score = new_best_score 
 
+        alpha = alpha * 0.9
+
         # run grid search and compute new best param
         print "Seaching params:", temp_param_grid
         clf = getModel()
-        searcher = GridSearchCV(clf, param_grid=temp_param_grid, scoring='log_loss', n_jobs=-1, pre_dispatch=2, verbose=6)
+        searcher = GridSearchCV(clf, param_grid=temp_param_grid, scoring='log_loss', n_jobs=-1, pre_dispatch=2, verbose=6, cv=2)
         start = time()
         searcher.fit(data, target)
         print("GridSearchCV took %.2f seconds for %d candidate parameter settings."
@@ -96,18 +99,23 @@ def auto_grid_search(data, target):
         new_param_grid = {}
 
         key = param_queue[0]
-        if searcher.best_params_[key] is not best_param_grid[key]:
+        print "Changing ", key , "from", best_param_grid[key], "to", searcher.best_params_[key]
+        if str(searcher.best_params_[key]) != str(best_param_grid[key]):
+            print type(searcher.best_params_[key])
+            print type(best_param_grid[key])
             # 1. If this was improved, keep searching
             best_value = searcher.best_params_[key]
-            new_values = get_neighbor_values(best_value)
+            new_values = get_neighbor_values(best_value, alpha)
             new_param_grid[key] = new_values
         else:
             # 2. find the next one to tune
             param_queue = param_queue[1:] + param_queue[:1]
             key = param_queue[0] 
             best_value = searcher.best_params_[key]
-            new_values = get_neighbor_values(best_value)
+            new_values = get_neighbor_values(best_value, alpha)
             new_param_grid[key] = new_values
+
+        print "Tuning key:", key
 
         # 3. Fill in best params for the rest 
         for key in best_param_grid:
