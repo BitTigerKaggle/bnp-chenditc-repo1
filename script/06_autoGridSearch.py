a2b46a6e8c8ced203f7d56d28b005796c18447fa
EC2 Default User <ec2-user@ip-172-31-10-101.us-west-2.compute.internal>
2016-04-15 12:46:17 +0000

    Remove PCA resisual feature

---
+++ script/06_autoGridSearch.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python -u
+
+from sklearn.decomposition import PCA
+
+from base import *
+
+
+def addPCAFeature(df):
+    features = df.select_dtypes(['float64'])
+
+    print "input feature shape:", features.shape
+
+    component_number = 80
+
+    pca = PCA(n_components=component_number)
+    pca.fit(features)
+
+    pca_feature = pca.transform(features)
+    pca_residual = features - pca.inverse_transform(pca_feature)
+
+    print "pca_feature shape:", pca_feature.shape
+    print "pca_residual shape:", pca_residual.shape
+
+    for column in features.columns:
+        df['pca_residual_' + column] = pca_residual[column]
+
+
+print "reading data"
+df = pandas.read_csv('./train.csv')
+
+print "reprocessing"
+
+df = addMissingPattern(df)
+
+fillNull(df)
+
+convertToNumeric(df)
+
+auto_grid_search(df.drop(['ID','target'], axis=1), df['target']) 
